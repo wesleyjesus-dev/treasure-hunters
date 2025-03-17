d04335dac4f9eac34099bad4d71a9269e6e0d7e9
wesley jesus <wesleydsjesus@gmail.com>
2025-03-16 22:22:40 -0300

    added Gdunit in project

---
+++ character/CharacterTests.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+using GdUnit4;
+using GdUnit4.Api;
+using GdUnit4.Core;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestSuite]
+public class CharacterTests
+{
+
+    [TestCase]
+    public async Task Teste()
+    {
+        var runner = ISceneRunner.Load("res://levels/island_t_1.tscn");
+
+        runner.SimulateKeyPressed(Key.Space);
+
+        Console.WriteLine("teste");
+
+        await runner.AwaitIdleFrame();
+    }
+
+}
